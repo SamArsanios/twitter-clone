9889da2f5677e284295538d80f6775eb2c2023eb
SamArsanios <samson.kibrom@hotmail.com>
2021-02-18 12:29:49 +0300

    Fix linter errrors

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TweetsController < ApplicationController
   before_action :set_tweet, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: %i[index show]
